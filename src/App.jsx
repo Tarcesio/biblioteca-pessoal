7c48416b4f357a3754ba0a74e93c478301a3d016
Tarcisio Bim <tarcibim@gmail.com>
2026-07-18 12:23:37 -0300

    Página criada
    Página de fallback chamada NotFound foi criada.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Main from './components/Main'
 import TodoListPage from './pages/todo-list/TodoListPage'
 import InteractiveColors from './pages/interactiveColors/interactiveColors'
 import { Routes, Route } from 'react-router-dom'
+import NotFound from './pages/NotFound'
 
 export default function App() {
   return (
@@ -13,6 +14,11 @@ export default function App() {
         <Route path="/" element={<Main />} />
         <Route path="/todo-list" element={<TodoListPage />} />
         <Route path="/interactive-colors" element={<InteractiveColors />} />
+
+
+        // Rota de fallback para páginas não encontradas (404) [SEMPRE POR ULTIMA]
+        <Route path="*" element={<NotFound />} />
+
       </Routes>
     </>
   )
